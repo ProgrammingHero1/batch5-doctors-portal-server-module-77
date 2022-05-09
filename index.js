26d57c6722e752aac63ac80743aad651d5f17365
Programming Hero <programming.hero1@gmail.com>
2022-05-08 23:06:49 -0500

    created service API

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://doctor_admin:<password>@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello From Doctor Uncle!')
