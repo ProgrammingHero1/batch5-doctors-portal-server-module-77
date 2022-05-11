59d2f4d86940a5c39e7c0fd570ee15415c93bb74
Programming Hero <programming.hero1@gmail.com>
2022-05-11 17:24:55 -0500

    added booking post api

--- index.js
+++ index.js
@@ -19,14 +19,35 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async(req, res) =>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
+        
+        /**
+         * API Naming Convention
+         * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+         * app.get('/booking/:id') // get a specific booking 
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id) //
+         * app.delete('/booking/:id) //
+        */
+
+        app.post('/booking', async(req, res) =>{
+          const booking = req.body;
+          const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+          const exists = await bookingCollection.findOne(query);
+          if(exists){
+            return res.send({success: false, booking: exists})
+          }
+          const result = await bookingCollection.insertOne(booking);
+          return res.send({success: true, result});
+        })
 
     }
     finally{
