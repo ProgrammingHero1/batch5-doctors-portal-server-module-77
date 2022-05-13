b1f6cb78b24325a695b99bcb9588abdca7316067
Programming Hero <programming.hero1@gmail.com>
2022-05-13 09:39:31 -0500

    added admin route

--- index.js
+++ index.js
@@ -45,9 +45,34 @@ async function run() {
       res.send(services);
     });
 
-    app.get('/user', async(req, res) =>{
+    app.get('/user', verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
+    });
+
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin})
+    })
+
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
+
     })
 
     app.put('/user/:email', async (req, res) => {
@@ -109,8 +134,8 @@ async function run() {
         const bookings = await bookingCollection.find(query).toArray();
         return res.send(bookings);
       }
-      else{
-        return res.status(403).send({message: 'forbidden access'});
+      else {
+        return res.status(403).send({ message: 'forbidden access' });
       }
     })
 
